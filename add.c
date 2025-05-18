63184d1a223bb6540da9f10ae3bf7b1bd02f4b6a
Pavithra <pavithra@example.com>
2025-05-19 00:18:37 +0530

    test commit

--- add.c
+++ add.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
-int main() {
-    int num1, num2, sum;
+int main() 
+
+{
+    int num1, num2, num3, sum;
 
     // Asking for user input
-    printf("Enter two integers: ");
-    scanf("%d %d", &num1, &num2);
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
 
     // Adding the two numbers
-    sum = num1 + num2;
+    sum = num1 + num2 + num3;
 
     // Displaying the result
     printf("Sum: %d\n", sum);
 
     return 0;
+
 }
